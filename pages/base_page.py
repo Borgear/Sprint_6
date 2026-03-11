bdc286417621198f75ac1320b5cfd692a2886350
Dmitry Baryshev <d_borg@mail.ru>
2026-03-11 18:37:41 +0300

    3-rd commit

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,19 +14,27 @@ class BasePage:
     def click_element(self, locator):
         element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator))
         element.click()
-
+    
+    @allure.step("Получить текст элемента: {locator}")
+    def get_text_from_element(self, locator):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+        return element.text
+    
     @allure.step("Прокрутка до элемента: {locator}")
     def scroll_to_element(self, locator):
-        element = self.find_element(locator)
-        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(locator))
+        self.driver.execute_script('arguments[0].scrollIntoView({block: "center", inline: "nearest"});', element)
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator))
     
     @allure.step("Ожидание и принятие куки")
     def wait_cookie_click(self, locator):
         self.click_element(locator)
     
-    @allure.step("Ожидание появления в URL строки: {url_part}")
-    def wait_for_url_contains(self, url_part):
-        return WebDriverWait(self.driver, 10).until(EC.url_contains(url_part))
+    @allure.step("Переключиться на новую вкладку и дождаться URL: {expected_url}")
+    def switch_to_new_window_and_wait_url(self, expected_url):
+        WebDriverWait(self.driver, 10).until(lambda d: len(d.window_handles) > 1)
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        WebDriverWait(self.driver, 10).until(EC.url_contains(expected_url))
     
     @allure.step("Получить текущий URL")
     def get_current_url(self):
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,34 +1,33 @@
 import allure
 from pages.base_page import BasePage
 from locators import MainPageLocators
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 class MainPage(BasePage):
     @allure.step("Открыть вопрос в FAQ под индексом {index}")
     def click_question(self, index):
-        loc = (MainPageLocators.QUESTION[0], MainPageLocators.QUESTION[1].format(index))
+        method, locator_string = MainPageLocators.QUESTION
+        loc = (method, locator_string.format(index))
         self.scroll_to_element(loc)
         self.click_element(loc)
 
     @allure.step("Получить текст ответа в FAQ под индексом {index}")
     def get_answer_text(self, index):
-        loc = (MainPageLocators.ANSWER[0], MainPageLocators.ANSWER[1].format(index))
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(loc)).text
-        return self.find_element(loc).text
+        method, locator_string = MainPageLocators.ANSWER
+        loc = (method, locator_string.format(index))
+        return self.get_text_from_element(loc)
 
     @allure.step("Нажать на выбранную кнопку заказа")
     def click_order_button(self, locator):
         self.scroll_to_element(locator)
         self.click_element(locator)
+    
     @allure.step("Нажать на логотип 'Самокат'")
     def click_logo_scooter(self):
         self.click_element(MainPageLocators.LOGO_SCOOTER)
 
     @allure.step("Нажать на логотип 'Яндекс' и переключиться на новую вкладку")
     def check_yandex_redirect(self, expected_url):
-        self.driver.switch_to.window(self.driver.window_handles[-1])
-        self.wait_for_url_contains(expected_url)
+        self.click_element(MainPageLocators.LOGO_YANDEX)
+        self.switch_to_new_window_and_wait_url(expected_url)
         return expected_url in self.get_current_url()
     
